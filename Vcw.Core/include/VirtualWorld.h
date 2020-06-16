8503566b6845bcfd440c0c03daa2ed7f659aa623
msours <michaelsours2@gmail.com>
2020-06-15 20:25:18 -0400

    Added main method for computing each cameras perspective of a virtual prop

--- Vcw.Core/include/VirtualWorld.h
+++ Vcw.Core/include/VirtualWorld.h
@@ -5,12 +5,16 @@
 
 namespace Vcw
 {
+    struct CameraPerspective;
+
     class VirtualWorld
     {
         public:
 
             VirtualWorld() {}
             VirtualWorld(const std::vector<VirtualCamera> &VirtualCameras);
+
+            std::vector<CameraPerspective> ComputeAllCameraPerspectivesOfProp(const VirtualProp &virtualProp);
 
             void AddCamera(const VirtualCamera &virtualCamera);
             void RemoveCamera(const int CameraID);
@@ -24,6 +28,21 @@ namespace Vcw
         private:
 
             int MaxCameraID = -1;
+    };
+
+    struct CameraPerspective
+    {
+        CameraPerspective() {}
+        CameraPerspective(const cv::Mat &Image, const int CameraID) : Image(Image), CameraID(CameraID) { }
+
+        CameraPerspective(const CameraPerspective &cameraPerspective)\
+        {
+            this->Image = cameraPerspective.Image;
+            this->CameraID = cameraPerspective.CameraID;
+        }
+
+        cv::Mat Image;
+        int CameraID;
     };
 }
 
--- Vcw.Core/source/VirtualWorld.cpp
+++ Vcw.Core/source/VirtualWorld.cpp
@@ -9,11 +9,28 @@ namespace Vcw
         for(k = 0; k < VirtualCameras.size(); k++) this->VirtualCameras[k].ID = k;
         this->MaxCameraID = this->VirtualCameras.back().ID;
     }
+
+    std::vector<CameraPerspective> VirtualWorld::ComputeAllCameraPerspectivesOfProp(const VirtualProp &virtualProp)
+    {
+        std::vector<CameraPerspective> cameraPerspectives = std::vector<CameraPerspective>(this->VirtualCameras.size());
+
+        std::vector<int> Iterations(cameraPerspectives.size());
+
+        std::iota(std::begin(Iterations), std::end(Iterations), 0);
+        std::for_each(std::execution::par_unseq, Iterations.begin(), Iterations.end(), [&cameraPerspectives, virtualProp, this](auto&& k)
+        {
+            cameraPerspectives[k] = CameraPerspective(this->VirtualCameras[k].ComputeCameraPerspectiveOfProp(virtualProp), this->VirtualCameras[k].ID);
+        });
+
+        return cameraPerspectives;
+    }
+
     void VirtualWorld::AddCamera(const VirtualCamera &virtualCamera)
     {
         this->VirtualCameras.push_back(virtualCamera);
         this->VirtualCameras.back().ID = ++this->MaxCameraID;
     }
+
     void VirtualWorld::RemoveCamera(const int CameraID)
     {
        this->VirtualCameras.erase(
@@ -22,15 +39,18 @@ namespace Vcw
                     }), this->VirtualCameras.end());
 
     }
+
     void VirtualWorld::MoveCamera(const int CameraID, const cv::Affine3d &Room_T_Camera_New)
     {
         for(int k = 0; k < this->VirtualCameras.size(); k++) if(VirtualCameras[k].ID == CameraID) VirtualCameras[k].Room_T_Camera = Room_T_Camera_New;
     }
+
     VirtualCamera VirtualWorld::FindCamera(const int CameraID) const
     {
         for(int k = 0; k < this->VirtualCameras.size(); k++) if(VirtualCameras[k].ID == CameraID) return VirtualCameras[k];
         return VirtualCamera();
     }
+
     cv::Affine3d VirtualWorld::FindTransformBetweenCameras(const int CameraID_0, const int CameraID_1) const
     {
         return FindCamera(CameraID_0).Room_T_Camera.inv() * FindCamera(CameraID_1).Room_T_Camera;
