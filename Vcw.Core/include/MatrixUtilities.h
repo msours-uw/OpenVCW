61fa4b091790e50a89342bcd3e8013a2a8085a03
msours <michaelsours2@gmail.com>
2020-07-11 22:28:28 -0400

    fixed indenting

--- Vcw.Core/include/MatrixUtilities.h
+++ Vcw.Core/include/MatrixUtilities.h
@@ -8,18 +8,18 @@ typedef unsigned char byte;
 
 namespace Vcw
 {
-	class MatrixUtilities final
-	{
-	public:
+    class MatrixUtilities final
+    {
+    public:
 
-		static byte BilinearInterpolate(const cv::Mat &Image, const cv::Point2d &Probe);
+        static byte BilinearInterpolate(const cv::Mat &Image, const cv::Point2d &Probe);
 
-		static cv::Affine3d CreateAffineTransform(const double Rx, const double Ry, const double Rz, const double Tx, const double Ty, const double Tz);
+        static cv::Affine3d CreateAffineTransform(const double Rx, const double Ry, const double Rz, const double Tx, const double Ty, const double Tz);
 
         static cv::Point2d Normalize(const cv::Point3d &Point);
 
-	private:
+    private:
 
-		MatrixUtilities() = delete;
-	};
+        MatrixUtilities() = delete;
+    };
 }
